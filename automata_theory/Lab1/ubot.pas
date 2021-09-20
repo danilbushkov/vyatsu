c94a3288970f7988808e819da863e43cd9101d66
danilbushkov <danil1357997@gmail.com>
2021-09-20 21:42:45 +0300

    checkers moves

--- automata_theory/Lab1/ubot.pas
+++ automata_theory/Lab1/ubot.pas
@@ -20,7 +20,7 @@ uses
   Classes, SysUtils,checkerunit;
 
 procedure moveBot();
-function checkMove(x,y:integer;var location:tlocation):boolean;
+function checkMoveCapture(x,y:integer;var location:tlocation):boolean;
 
 implementation
 
@@ -36,19 +36,87 @@ begin
          begin
              if checkPlayer(location[i,j],player) then
              begin
-               if checkMove(i,j,location) then
-               begin
-
-               end;
+               //if checkMove(i,j,location) then
+               //begin
+               //
+               //end;
              end;
          end;
      end;
 
 end;
 
+procedure BCapture(var crd:tcrd;  //координаты
+                 var location:Tlocation; //поле
+                 var move:Tmove;
+                 var moves:Tmoves;
+                 var depth:integer;   //глубина рукурсии
+                 var i:integer);
+begin
 
-//Проверить на ход
-function checkMove(x,y:integer;var location:tlocation):boolean;
+
+end;
+
+//Получить возможную рубку
+function GetMove(crd:tcrd;  //координаты
+                 location:Tlocation; //поле
+                 move:Tmove;
+                 moves:Tmoves;
+                 depth:integer;   //глубина рукурсии
+                 var i:integer):boolean;  //количество ходов
+var x,y:integer;
+  a:boolean;
+begin
+     a:=false;
+     x:=crd.cellx;
+     y:=crd.celly;
+     //левый верхний угол
+     if( (x-2)>0 ) and ( (y-2)>0 ) then
+     begin
+        if(location[x-2,y-2] = 0) and (checkPlayer(location[x-1,y-1],1)) then
+        begin
+           BCapture(crd,location,move,moves,depth,i);
+           a:=true;
+        end;
+     end;
+
+     //правый верхний угол
+     if( (x+2)<8 ) and ( (y-2)>0 ) then
+     begin
+        if(location[x+2,y-2] = 0) and (checkPlayer(location[x+1,y-1],1)) then
+        begin
+            BCapture(crd,location,move,moves,depth,i);
+            a:=true;
+        end;
+     end;
+
+     //левый нижний угол
+     if( (x-2)>0 ) and ( (y+2)<8 ) then
+     begin
+        if(location[x-2,y+2] = 0) and (checkPlayer(location[x-1,y+1],1)) then
+        begin
+            BCapture(crd,location,move,moves,depth,i);
+            a:=true;
+        end;
+     end;
+
+     //правый нижний угол
+     if( (x+2)<8 ) and ( (y+2)<8 ) then
+     begin
+        if(location[x+2,y+2] = 0) and (checkPlayer(location[x+1,y+1],1)) then
+        begin
+             BCapture(crd,location,move,moves,depth,i);
+             a:=true;
+        end;
+     end;
+
+
+     Exit(a);
+end;
+
+
+//Проверить на Рубку
+function checkMoveCapture(x,y:integer;var location:tlocation):boolean;
 begin
      //проверить рубку
 
@@ -88,6 +156,13 @@ begin
         end;
      end;
 
+
+     Exit(false);
+
+end;
+
+function checkMoveSimple(x,y:integer;var location:tlocation):boolean;
+begin
      //проверка шага
      //левый нижний угол
      if( (x-1)>0 ) and ( (y+1)<8 ) then
