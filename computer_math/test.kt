084f2cef30420c95a077e6bdea984329c1ca326e
danilbushkov <danil1357997@gmail.com>
2021-10-16 18:04:18 +0300

    Corrected the laboratory on computational mathematics

--- computer_math/test.kt
+++ computer_math/test.kt
@@ -11,7 +11,11 @@ fun Hord(a:Double, b:Double, f: (Double)->Double):Double =
         b - f(b) * (b-a)/(f(b)-f(a))
 
 fun Canon(x: Double) : Double = 2 - ln(1+2*x)
-fun CanonDerivative(x: Double) : Double = -(2/(1+2*x))
+fun CanonDerivative(x: Double) : Double{
+    var k=2.0
+    
+    return (-1*FirstDerivative(x)/k)
+}
 
 fun isolate(a:Double, _left:Double, _right:Double, func: (Double)->Double)
 : Array<Double>{
@@ -49,11 +53,11 @@ fun iter_m(
     var arr = mutableListOf<Array<Double>>()
     var q:Double
 
-    //k=-1/D1(a)
-
+    k = max(FirstDerivative(0.5),FirstDerivative(1.4))
+    //println(k)
     do{
         b = a
-        a = Canon(a)//a + k*Func(a)
+        a = a - Func(a)/k
         d = abs(a - b) 
 
         arr.add(arrayOf(b,a,d))
@@ -74,6 +78,8 @@ fun com_m(l:Double,
     var a: Double
     var b: Double
     var d: Double
+    var a1:Double
+    var b1:Double
     var arr = mutableListOf<Array<Double>>()
     if(f){
         b=l
@@ -83,7 +89,9 @@ fun com_m(l:Double,
         a=l
     }
     do{
-        
+        a1=a
+        b1=b
+
         if(f){
             b = Hord(a,b,::Func)
             a = a - Func(a)/D1(a)
@@ -92,7 +100,7 @@ fun com_m(l:Double,
             b = b - Func(b)/D1(b)
         }
         d=abs(b-a)
-        arr.add(arrayOf(a,b,d))
+        arr.add(arrayOf(a1,b1,a,b,d))
         
     }while (d>eps)
     return  arr
@@ -104,13 +112,13 @@ fun main() {
     var ml1 = com_m(0.5,1.4, 0.00001,::Func,::FirstDerivative,::SecondDerivative)
     var ml = iter_m(0.5,1.4, 0.00001,::Func,::FirstDerivative)
     var i:Int=0
-
-
+    var res:Double=0.0;
+    var j:Int=0
     println("Function: " + function)
     println("[0,5; 1,4]")
     print("Phi'(a) = " + FirstDerivative(0.5))
     println(" Phi'(b) = " + FirstDerivative(1.4))
-    println("Canonical function: 2 - ln(1+2*x)")
+    
     if(CanonDerivative(0.5)<0.0){
         println("Two-way convergence -> |xn+1 - xn| <= eps")
     }else{
@@ -118,27 +126,55 @@ fun main() {
     }
 
     println("Combined method:")
-    println("n       Xn      Xn+1       |Xn-Xn+1|")
+    if(Func(0.5) * SecondDerivative(0.5) > 0){
+        println("Excess - Chord method")
+        println("Flaw - Tanget method")
+    }else{
+        println("Excess - Tanget method")
+        println("Flaw - Chord method")
+    }
+    println("f(a) = " + Func(0.5))
+    println("f(b) = " + Func(1.4))
+    println("f'(a) = " + FirstDerivative(0.5))
+    println("f'(b) = " + FirstDerivative(1.4))
+    println("f''(a) = " + SecondDerivative(0.5))
+    println("f''(b) = " + SecondDerivative(1.4))
+    println("xa                xb          f(xa)       f(xb)      |f(xa)-f(xb)|")
+
     for(ar in ml1){
         print(i.toString() + " ")
+        j=0
         for(a in ar){
-            print("%.6f ".format(a))
+            print("%.6f      ".format(a))
+            j++
+            if(j==4){
+                res=a
+            }
         }
         println()
         i++
     }
+    println("Result = "+res)
     println()
     i=0
     
     println("Iteration method:")
+    println("phi'(a) = " + CanonDerivative(0.5))
+    println("phi'(b) = " + CanonDerivative(1.4))
     println("n       Xn      Xn+1       |Xn-Xn+1|")
     for(ar in ml){
         print(i.toString()+" ")
         for(a in ar){
             print("%.6f ".format(a))
+            j++
+            if(j==4){
+                res=a
+            }
         }
         println()
+        
         i++
     }
+    println("Result = "+res)
 
 }
