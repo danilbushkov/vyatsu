d58cb4abf6a0672f40b7bbf0104eeaca78c69ad8
danilbushkov <danil1357997@gmail.com>
2021-12-07 20:37:30 +0300

    added Gauss method in 4 lab

--- computer_math/4lab/4lab.pas
+++ computer_math/4lab/4lab.pas
@@ -5,13 +5,17 @@ uses math;
 type
     TFunc=function(x:real):real; 
     TRange=array[0..1] of real;
-
+    TGaussRow=array of real;
+    TGaussArray=array of TGaussRow;
 const
     RangeT:TRange=(0.2,1.2);
     RangeS:TRange=(0.6,1.4);
+    RangeG:TRange=(2.2,3.4);
 
 var result1:array of array[0..1] of  real;
-var resultS:array of array[0..1] of real;
+    resultS:array of array[0..1] of real;
+    pointsGauss1:TGaussArray;
+    pointsGauss2:TGaussArray;
 
 function TrapezoidDerivarion(x:real):real;
 begin
@@ -26,6 +30,11 @@ end;
 function FuncTrapezoid(x:real):real;
 begin
     Exit(1/sqrt(x*x+1));
+end;
+
+function FuncGauss(x:real):real;
+begin
+    Exit( x*x/sqrt(x+1) );
 end;
 
 procedure PrintResult1();
@@ -104,6 +113,7 @@ begin
     n:=round((b-a)/h);
     setlength(result1,n);
     writeln('Trapezoids: ');
+    writeln('f(x)=1/sqrt(x^2+1)  ');
     write('Range: ');writeln('[',a:5:4,',',b:5:4,']');
     write('e = ');writeln(eps:5:4);
     write('h = ');writeln(h:5:4);
@@ -189,7 +199,7 @@ begin
     setlength(resultS,n);
     
     writeln('Simpson: ');
-    
+        writeln('f(x)=cos(x)/(x+1)');
     
         write('Step: h');
         write('Range: ');writeln('[',a:5:4,',',b:5:4,']');
@@ -230,10 +240,146 @@ begin
         writeln(R2:7:6);
 end;
 
+procedure InitGauss();
+begin
+    pointsGauss1:=TGaussArray.Create(
+        TGaussRow.Create(-0.86114,-0.33998,0.33998,0.86114),
+        TGaussRow.Create(0.34785,0.65215,0.65215,0.34785)
+    );
+
+    pointsGauss2:=TGaussArray.Create(
+        TGaussRow.Create(-0.949107912,
+                        -0.741531186,
+                        -0.405845151,
+                        0,
+                        0.405845151,
+                        0.741531186,
+                        0.949107912),
+        TGaussRow.Create(
+            0.129484966,
+            0.279705391,
+            0.381830051,
+            0.417959184,
+            0.381830051,
+            0.279705391,
+            0.129484966)
+    );
+end;
+
+procedure PrintTableG(points:TGaussArray;table:TGaussArray);
+var i,j:integer;
+begin
+    write('i':4);
+    write('xi':13);
+    write('Ai':13);
+    write('Argi':13);
+    write('f(argi)':13);
+    writeln('Ai*f(argi)':13);
+    for i:=0 to length(points[0])-1 do 
+    begin
+        write(i:4);
+        write(points[0][i]:13:9);
+        write(points[1][i]:13:9);
+        write(table[0][i]:13:9);
+        write(table[1][i]:13:9);
+        write(table[2][i]:13:9);
+        writeln();
+    end;
+    writeln();
+end;
+
+procedure PrintNodes(points:TGaussArray);
+var i:integer;
+begin
+    for i:=0 to length(points[0])-1 do
+    begin
+        write('x',i+1,'=',points[0][i]:13:9,'   ');
+        writeln('A',i+1,'=',points[1][i]:13:9);
+    end;
+end;
+
+function Gauss(Func:TFunc;a,b:real;points:TGaussArray):real;
+var i:integer;
+    sum:real;
+    arg:real;
+    fx:real;
+    afx:real;
+    m:real;
+    result:real;
+    table:TGaussArray;
+begin
+    sum:=0;
+    setlength(table,3);
+    for i:=0 to 2 do
+    begin
+        setlength(table[i],length(points[0]));
+    end;
+    for i:=0 to length(points[0])-1 do
+    begin
+        arg:=( (b-a)/2 )*points[0][i]+(a+b)/2;
+        table[0][i]:=arg;
+        fx:=Func(arg);
+        table[1][i]:=fx;
+        afx:=fx*points[1][i];
+        table[2][i]:=afx;
+        sum:=sum+afx;
+        
+    end;
+    PrintTableG(points,table);
+    writeln('sum: ',sum:10:9);
+
+    m:=(b-a)/2;
+    writeln('(b-a)/2=',m:10:9);
+
+    result:=m*sum;
+    writeln;
+    Writeln('Result: ',result:10:9);
+    Exit(result);
+end;
+
+
+procedure GaussTask(Func:TFunc;range:TRange);
+var a,b:real;
+    r1,r2:real;
+begin
+    InitGauss();
+    a:=range[0];
+    b:=range[1];
+    writeln('Gauss:');
+    writeln('f(x)= x^2/sqrt(x+1)');
+    writeln();
+    writeln('4 nodes:');
+    PrintNodes(pointsGauss1);
+    writeln();
+
+    writeln('7 nodes:');
+    PrintNodes(pointsGauss2);
+    writeln();
+
+
+    writeln();
+    write('Range: ');writeln('[',a:5:4,',',b:5:4,']');
+    writeln('For 4 nodes');
+    writeln('n=',length(pointsGauss1[0]));
+    writeln;
+    
+    r1:=Gauss(Func,a,b,pointsGauss1);
+
+
+    writeln;
+    writeln('For 7 nodes');
+    writeln('n=',length(pointsGauss2[0]));
+    writeln;
+    
+    r2:=Gauss(Func,a,b,pointsGauss2);
+    writeln();
+    Writeln('Inaccuracy: ',abs(r2-r1):11:9);
+
+end;
 
 
 begin
     //Trapezoids(@FuncTrapezoid,@TrapezoidDerivarion,RangeT,0.0001);
-    SimpsonRunge(@SimpsonFunc,RangeS,0.0001);
-
+    //SimpsonRunge(@SimpsonFunc,RangeS,0.0001);
+    GaussTask(@FuncGauss,RangeG);
 end.
