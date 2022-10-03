ac45610e2e69c7e24ad0dbcc405054b80ad3bb25
danilbushkov <danil1357997@gmail.com>
2022-10-03 18:00:21 +0300

    add lab2 for operations research

---
+++ 5_semester/operations_research/2lab/src/main.rs
@@ -0,0 +1,55 @@
+mod simplex;
+mod parse;
+mod input;
+
+
+
+//use crate::simplex_data::SimplexData;
+use crate::parse::parse;
+use crate::input::input;
+
+
+fn main() {
+    let input = input();
+    let mut data = parse(input);
+    data.preparatory_stage();
+    data.form_basis();
+
+    let mut work = true;
+    while work {
+        data.calculate_deltas();
+        println!("{}", data);
+        println!("{:=>40}", "=");
+        if data.check_optimality() {
+            work = false;
+            if data.check_artificial_variables_in_basis() {
+                let s = "It doesn't have a correct solution because the solution contains artificial variables!";
+                println!("{}", s);
+            } else {
+                data.print_result();
+            }
+        } else {
+            if !data.move_to_optimal_solution() {
+                println!("The function is not limited. There is no optimal solution!");
+                work = false;
+            }
+        }
+        
+    }
+    
+
+
+
+    
+
+
+}
+
+
+
+
+
+
+
+
+
