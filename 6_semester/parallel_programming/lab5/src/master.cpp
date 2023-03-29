d68d79b818873c3ca00d8ee27a04338251814918
danilbushkov <danil1357997@gmail.com>
2023-03-29 21:33:41 +0300

    added send data in slave and back

--- 6_semester/parallel_programming/lab5/src/master.cpp
+++ 6_semester/parallel_programming/lab5/src/master.cpp
@@ -1,24 +1,183 @@
 #include <pvm3.h>
 #include <iostream>
+#include <vector>
+#include <complex>
+#include <fstream>
 
-using namespace std;
+void get_polys(std::ifstream &in, std::vector<double> &poly1, std::vector<double> &poly2) {
+    int n = 0;
+    in >> n;
+    poly1.resize(n);
+    poly2.resize(n);
+    for(int i = 0; i < n; i++) {
+        in >> poly1[i];
+    }
+    for(int i = 0; i < n; i++) {
+        in >> poly2[i];
+    }
+}
+
+void set_result(std::ofstream &out, std::vector<double> &poly) {
+    out << poly.size() << '\n';
+    
+    for(int i = 0; i < poly.size(); i++) {
+        out << poly[i] << ' ';
+    }
+}
+
+
+
+void dpoly_to_cpoly(std::vector<double> poly, int rn, std::complex<double> *result) {
+    int n = poly.size();
+    for(int i = 0; i < n; i++) {
+        result[i] = std::complex<double>(poly[i], 0);
+    }
+}
+
+void cpoly_to_dpoly(int n, std::complex<double> *poly, std::vector<double> &result) {
+    
+    result.resize(n);
+    for(int i = 0; i < n; i++) {
+        result[i] = real(poly[i]);
+    }
+}
+
+
+bool has_one_bit(int num) {
+    int count = 0;
+    while(num > 0) {
+        count = count + (num & 1);
+        num = num >> 1;
+    }
+
+    return count == 1;
+}
+
+
+void fft(int tid, int n, std::complex<double> *poly, double s) {
+
+    int msgtag = 1;
+
+    double *data = (double *) poly;
+
+    
+
+    pvm_initsend(PvmDataDefault);
+    pvm_pkint(&n, 1, 1);
+    pvm_send(tid, msgtag);
+    pvm_pkdcplx(data, n, 1);
+    pvm_send(tid, msgtag);
+    pvm_pkdouble(&s, 1, 1);
+    pvm_send(tid, msgtag);
+
+    pvm_recv(tid, msgtag);
+    pvm_upkdcplx(data, n, 1);
+    
+
+}
+
+
+
+void fft_mult(
+    int ntids, int *tids,
+    std::vector<double> &poly1, std::vector<double> &poly2, std::vector<double> &result) {
+    int n = poly1.size();
+    int m = poly2.size();
+
+    if(n == 0 || m == 0) {
+        return;
+    }
+
+    int size = 0;
+    if(n > m) {
+        size = n;
+    } else {
+        size = m;
+    }
+
+    while(!has_one_bit(size)) {
+        size++;
+    }
+
+    size *= 2;
+
+    std::complex<double> *cpoly1 = new std::complex<double>[size];
+    std::complex<double> *cpoly2 = new std::complex<double>[size];
+    std::complex<double> *cresult = new std::complex<double>[size];
+
+
+    for(int i = 0; i < size; i++) {
+        std::cout << cpoly1[i] << std::endl;
+    }
+
+    dpoly_to_cpoly(poly1, size, cpoly1);
+    dpoly_to_cpoly(poly2, size, cpoly2);
+
+
+    
+    
+
+    fft(tids[0], size, cpoly1, 1);
+    fft(tids[1], size, cpoly2, 1);
+
+    for(int i = 0; i < size; i++) {
+        cresult[i] = (cpoly1[i] * cpoly2[i]) / std::complex<double>(size, 0);
+    }
+
+    
+    fft(tids[2], size, cresult, -1);
+
+    cpoly_to_dpoly(size, cresult, result);
+
+
+}
+
+
+
 
 int main(int argc, char** argv) {
-    int cc, tid, msgtag;
-    char buf[100];
-    cout << pvm_mytid() << endl;
+    int cc;
+    int tids[3];
+    const std::string path = std::string(getenv("HOME")) + "/tmp/test";
     
-    cc = pvm_spawn("slave", (char**)0, 0, "", 1, &tid);
-    
-    if(cc == 1) {
-        msgtag = 1;
-    
-        pvm_recv(tid, msgtag);
-        pvm_upkstr(buf);
-        cout << tid << ":" << buf;
-    } else {
-       cout << "Error" << endl;
+    std::ifstream in(path+"/input.txt");
+    std::ofstream out(path+"/output.txt");
+
+    std::vector<double> poly1;
+    std::vector<double> poly2;
+    std::vector<double> result;
+
+    if (in.is_open()) {
+        get_polys(in, poly1, poly2);
     }
+    in.close();
+    
+    
+
+    cc = pvm_spawn("slave", (char**)0, 0, "", 3, tids);
+    
+    if(cc == 3) {
+
+        fft_mult(3, tids, poly1, poly2, result);
+
+
+        
+    } else {
+       std::cout << "Error" << std::endl;
+    }
+    
+
+    std::cout << "sssssssssssss" << std::endl;
+
+
+    if (out.is_open()) {
+        set_result(out, result);
+    }
+    out.close();
+    
+
     pvm_exit();
+    
+    
     return 0;
 }
--- 6_semester/parallel_programming/lab5/src/slave.cpp
+++ 6_semester/parallel_programming/lab5/src/slave.cpp
@@ -2,21 +2,107 @@
 #include <iostream>
 #include <unistd.h>
 #include <cstring>
+#include <complex>
+
+int floor_power2(int num) {
+    int result = 0;
+    for(; num > 1; num=num>>1, result++);
+    return result;
+}
+
+
+int reverse_int(int num, int p) {
+    int result = 0;
+    for(int i = 0; i <= p; i++) {
+        result |= ( (num >> i) & 1) << (p - i);
+    }
+    return result;
+}
+
+bool has_one_bit(int num) {
+    int count = 0;
+    while(num > 0) {
+        count = count + (num & 1);
+        num = num >> 1;
+    }
+
+    return count == 1;
+}
+
+void fft_iterative(std::complex<double> *poly, int n, double s) {
+    if (n == 1)
+        return;
+    
+    for(int i = 0; i < n; i++) {
+        int rev_i = reverse_int(i, floor_power2(n-1));
+        
+        if(i < rev_i) {
+            swap(poly[i], poly[rev_i]);
+        }
+        
+    }
+    
+    for(int i = floor_power2(n)-1; i >= 0; i--) {
+        int num_seq = pow(2, i);
+        
+        double f = s*2 * M_PI / (n) * num_seq;
+        std::complex<double> wn = std::complex<double>(cos(f), sin(f));
+        
+        int num_items = n / num_seq;
+        for(int j = 0; j < num_seq; j++) {
+
+            
+            
+            std::complex<double> w = 1;
+            
+            int s = num_items*j;
+            int e = s + num_items/2;
+            
+            for(int k = s; k < e; k++) {
+                
+                std::complex<double> t = w * poly[k + num_items/2];
+                poly[k+num_items/2] = poly[k] - t;
+                poly[k] = poly[k] + t;
+                w *= wn;
+            }
+        }
+    }
+
+    
+}
 
 
 int main() {
-    int ptid, msgtag;
-    char buf[100] = "1000\0";
-    ptid = pvm_parent();
 
-    gethostname(buf + strlen(buf), 64);
-    msgtag = 1;
+    int msgtag = 1;
+    int ptid;
+    int n;
+    int s;
+    ptid = pvm_parent();
+    
+
+    pvm_recv(ptid, msgtag);
+    pvm_upkint(&n, 1, 1);
+    
+    double *poly = new double[n*2];
+
+    pvm_recv(ptid, msgtag);
+    pvm_upkdcplx(poly, n, 1);
+
+    pvm_recv(ptid, msgtag);
+    pvm_upkint(&s, 1, 1);
+    
+    fft_iterative((std::complex<double> *) poly, n, s);
+    
+    
     pvm_initsend(PvmDataDefault);
-    pvm_pkstr(buf);
+    pvm_pkdcplx(poly, n, 1);
     pvm_send(ptid, msgtag);
 
 
-
     pvm_exit();
+    
+    delete[] poly;
+
     return 0;
 }
