bc500d22d3c96635566bea6858ae56f682d69c8f
danilbushkov <danil1357997@gmail.com>
2024-02-28 20:21:42 +0300

    add text view

--- 8_semester/data_protection/lab3/src/main.rs
+++ 8_semester/data_protection/lab3/src/main.rs
@@ -7,29 +7,40 @@ mod tiger;
 use gtk::prelude::*;
 use gtk::{
     glib, Application, ApplicationWindow, Box, Button, Label, Orientation, Paned, Stack,
-    StackSidebar,
+    StackSidebar, TextBuffer, TextView,
 };
-use num::{BigInt, BigUint};
+use num::{BigInt, BigUint, Num};
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld1";
 
-fn main() {
-    //-> glib::ExitCode {
-    //let app = Application::builder().application_id(APP_ID).build();
-    //app.connect_activate(build_ui);
-    //app.run()
-    let (x, _, _) = rsa::gcd(BigInt::from(23), BigInt::from(2341));
-    println!("{}", x);
-    let v = rsa::sieve_of_eratosthenes(300);
-    println!("{:?}", v);
-    let keys = rsa::new_keys(BigUint::from(659 as u32), BigUint::from(599 as u32));
-    println!("{:?}", keys);
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+    app.connect_activate(build_ui);
+    app.run()
 
-    let m = "Привет!".as_bytes();
-    let c = rsa::encrypt(m, &keys.0);
-    println!("{:?} -> {:?}", m, c);
-    let m = rsa::decrypt(&c, &keys.1);
-    println!("{:?}", m);
+    //   let (x, _, _) = rsa::gcd(BigInt::from(23), BigInt::from(2341));
+    //   println!("{}", x);
+    //   let v = rsa::sieve_of_eratosthenes(300);
+    //   println!("{:?}", v);
+    //   let keys = rsa::new_keys(BigUint::from(659 as u32), BigUint::from(599 as u32));
+    //   println!("{:?}", keys);
+
+    //   let m = "Привет!".as_bytes();
+    //   let c = rsa::encrypt(m, &keys.0);
+    //   println!("{:?} -> {:?}", m, c);
+    //   println!();
+    //   let s: String = c
+    //       .iter()
+    //       .map(|v| v.to_str_radix(16))
+    //       .collect::<Vec<_>>()
+    //       .join(".");
+    //   println!("{}", s);
+    //   let c = s
+    //       .split('.')
+    //       .map(|s| BigUint::from_str_radix(s, 16).unwrap_or(BigUint::from(0 as u32)))
+    //       .collect::<Vec<_>>();
+    //   let m = rsa::decrypt(&c, &keys.1);
+    //   println!("{:?}", m);
 }
 
 fn build_ui(app: &Application) {
@@ -116,7 +127,24 @@ fn build_ui(app: &Application) {
         println!("\nText: {:?}; {}", bytes, bytes.len());
         println!("{}", d);
     });
+    let text_view = TextView::builder()
+        .margin_end(MARGIN)
+        .margin_top(MARGIN)
+        .margin_start(MARGIN)
+        .margin_bottom(MARGIN)
+        .wrap_mode(gtk::WrapMode::WordChar)
+        .build();
+    text_view.buffer().connect_changed(|b| {
+        const MAX: i32 = 256;
+        if b.char_count() > MAX {
+            let mut start = b.start_iter();
+            start.forward_chars(MAX);
+            let mut end = b.end_iter();
+            b.delete(&mut start, &mut end);
+        }
+    });
     task2_box.append(&task2_encrypt_button);
+    task2_box.append(&text_view);
 
     stack.add_titled(&task1_box, Some("task1_box"), "Задание 1");
     stack.add_titled(&task2_box, Some("task2_box"), "Задание 2");
